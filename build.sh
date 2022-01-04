cb3f0648d45031da8265ba7ab819200d43c591b5
Petr Novak <petr@umbr.cas.cz>
2022-01-04 23:03:44 +0100

    initial commit

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -x -e
+
+# General info
+DANTE_LTR_DIR=${PREFIX}/share/LTR_Finder
+R_PRG=extract_putative_ltr.R
+
+# Create folders
+mkdir -p ${PREFIX}/bin
+mkdir -p ${DANTE_LTR_DIR}
+cp -r * ${DANTE_LTR_DIR}
+
+# copy executables
+cp ${R_PRG} ${PREFIX}/bin/
+chmod a+x ${PREFIX}/bin/${R_PRG}
